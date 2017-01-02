4c5ba0212c49e17af24e3d4ff1eb6288c7d2a14f
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-01-03 07:02:44 +0800

    build: Define MSM_VIDC_TARGET_LIST
    Currently, in the media HAL, we're replacing the usage
    of this variable with a check for TARGET_BOARD_PLATFORM.
    
    Instead of having to do that every time a new branch is made,
    define the variable in qcom_target.
    
    Change-Id: Idd85b6f0b167bbc64833eb03335abcaa7b9de4a9

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -49,20 +49,26 @@ MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660
 MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
 
 ifeq ($(call is-board-platform-in-list, $(A_FAMILY)),true)
+    MSM_VIDC_TARGET_LIST := $(A_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8960
 else
 ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
+    MSM_VIDC_TARGET_LIST := $(B_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8974
 else
 ifeq ($(call is-board-platform-in-list, $(B64_FAMILY)),true)
+    MSM_VIDC_TARGET_LIST := $(B64_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8994
 else
 ifeq ($(call is-board-platform-in-list, $(BR_FAMILY)),true)
+    MSM_VIDC_TARGET_LIST := $(BR_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8916
 else
 ifeq ($(call is-board-platform-in-list, $(UM_FAMILY)),true)
+    MSM_VIDC_TARGET_LIST := $(UM_FAMILY)
     QCOM_HARDWARE_VARIANT := msm8996
 else
+    MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
     QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
 endif
 endif
