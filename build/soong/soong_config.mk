311e98f61eeeba1af7dbe494c5b400efafafaabf
Paul Keith <javelinanddart@gmail.com>
2018-09-21 00:52:58 +0200

    soong: Use add_json_str_omitempty directive for TARGET_LD_SHIM_LIBS
    * The code is ifdef'ed out, so let's match that expecation here
    
    Change-Id: I59c8e276c3dd28c4f46222f66d626e48b2aa6551

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -17,8 +17,9 @@ $(call add_json_bool,   Uses_qcom_um_4_4_family, 				    $(filter true,$(TARGET_
 $(call add_json_bool,   Uses_qcom_um_4_9_family, 				    $(filter true,$(TARGET_USES_QCOM_UM_4_9_FAMILY)))
 $(call add_json_bool,   Uses_qcom_um_4_14_family, 				    $(filter true,$(TARGET_USES_QCOM_UM_4_14_FAMILY)))
 
-$(call add_json_str_omitempty,	Additional_gralloc_10_usage_bits,	$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits,	$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 $(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
+$(call add_json_str_omitempty, Target_shim_libs,					$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 
 $(call add_json_val_default, Bootloader_message_offset,				$(BOOTLOADER_MESSAGE_OFFSET), 0)
 
@@ -26,7 +27,6 @@ $(call add_json_str,	QTIAudioPath,						        $(call project-path-for,qcom-aud
 $(call add_json_str,	QTIDisplayPath,						        $(call project-path-for,qcom-display))
 $(call add_json_str,	QTIMediaPath,						        $(call project-path-for,qcom-media))
 $(call add_json_str,	Specific_camera_parameter_library,			$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
-$(call add_json_str,	Target_shim_libs,					        $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
