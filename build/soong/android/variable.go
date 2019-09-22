RevengeOS/android_vendor_revengeos

vendor: Rewrite soong_config for Pie
Change-Id: I71ba2262026dda8688faeff712f57aa6c001c656

build/soong/android/variable.go
@@ -21,7 +21,7 @@ type Product_variables struct {
 	}
 
 	Qcom_bsp_legacy struct {
-		Cflags []string
+		Cppflags []string
 	}
 
 	Qti_flac_decoder struct {
@@ -50,19 +50,19 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
-	Mtk_hardware            *bool `json:",omitempty"`
-	QTIAudioPath            *string `json:",omitempty"`
-	QTIDisplayPath          *string `json:",omitempty"`
-	QTIMediaPath            *string `json:",omitempty"`
-	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
-	Qcom_bsp_legacy         *bool `json:",omitempty"`
-	Qti_flac_decoder        *bool `json:",omitempty"`
-	Target_uses_qsml      *bool `json:",omitempty"`
-	Target_uses_eigen       *bool `json:",omitempty"`
-	Target_shim_libs  *string `json:",omitempty"`
+	Has_legacy_camera_hal1						*bool `json:",omitempty"`
+	Uses_media_extensions						*bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library		*bool `json:",omitempty"`
+	Specific_camera_parameter_library			*string `json:",omitempty"`
+	Needs_text_relocations						*bool `json:",omitempty"`
+	Mtk_hardware								*bool `json:",omitempty"`
+	QTIAudioPath								*string `json:",omitempty"`
+	QTIDisplayPath								*string `json:",omitempty"`
+	QTIMediaPath								*string `json:",omitempty"`
+	Cant_reallocate_omx_buffers					*bool `json:",omitempty"`
+	Qcom_bsp_legacy								*bool `json:",omitempty"`
+	Qti_flac_decoder							*bool `json:",omitempty"`
+	Target_uses_qsml							*bool `json:",omitempty"`
+	Target_uses_eigen							*bool `json:",omitempty"`
+	Target_shim_libs							*string `json:",omitempty"`
 }

DU-X/android_vendor_extras

build/soong/android/variable.go
@@ -25,7 +25,7 @@ type Product_variables struct {
 	}
 
 	Qcom_bsp_legacy struct {
-		Cflags []string
+		Cppflags []string
 	}
 
 	Qti_flac_decoder struct {
@@ -42,18 +42,18 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Needs_legacy_camera_hal1_dyn_native_handle  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
-	Mtk_hardware            *bool `json:",omitempty"`
-	QTIAudioPath            *string `json:",omitempty"`
-	QTIDisplayPath          *string `json:",omitempty"`
-	QTIMediaPath            *string `json:",omitempty"`
-	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
-	Qcom_bsp_legacy         *bool `json:",omitempty"`
-	Qti_flac_decoder        *bool `json:",omitempty"`
-	Target_shim_libs  *string `json:",omitempty"`
+	Has_legacy_camera_hal1						*bool `json:",omitempty"`
+	Needs_legacy_camera_hal1_dyn_native_handle	*bool `json:",omitempty"`
+	Uses_media_extensions						*bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library		*bool `json:",omitempty"`
+	Specific_camera_parameter_library			*string `json:",omitempty"`
+	Needs_text_relocations						*bool `json:",omitempty"`
+	Mtk_hardware								*bool `json:",omitempty"`
+	QTIAudioPath								*string `json:",omitempty"`
+	QTIDisplayPath								*string `json:",omitempty"`
+	QTIMediaPath								*string `json:",omitempty"`
+	Cant_reallocate_omx_buffers					*bool `json:",omitempty"`
+	Qcom_bsp_legacy								*bool `json:",omitempty"`
+	Qti_flac_decoder							*bool `json:",omitempty"`
+	Target_shim_libs							*string `json:",omitempty"`
 }