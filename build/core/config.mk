RevengeOS/android_vendor_revengeos

build: Include qcom and mtk configs
* Include MTK/QCOM rules directly from the build/core/config.mk hook.
* Place main.mk hook and main_version.mk into build/core with the rest
  of the make system hooks.

Change-Id: Iad2f808f3e3eefbd2be2ca5facb907ff31babbb7

build/core/config.mk
@@ -1,0 +1,21 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#           (C) 2017-2018 The LineageOS Project
+#           (C) 2018 CarbonROM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Rules for MTK targets
+include $(TOPDIR)vendor/carbon/build/core/mtk_target.mk
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/carbon/build/core/qcom_target.mk

DU-X/android_vendor_extras

build/core/config.mk
@@ -1,0 +1,21 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#           (C) 2017-2018 The LineageOS Project
+#           (C) 2018 CarbonROM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Rules for MTK targets
+#include $(TOPDIR)vendor/extras/build/core/mtk_target.mk
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/extras/build/core/qcom_target.mk